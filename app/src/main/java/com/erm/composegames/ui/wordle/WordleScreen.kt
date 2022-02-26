daec00a90109b38f5b18a063dc726e9295d941bb
Elliot Mitchell <emitchell@microsoft.com>
2022-02-25 21:47:32 -0700

    UI rigged up

--- app/src/main/java/com/erm/composegames/ui/wordle/WordleScreen.kt
+++ app/src/main/java/com/erm/composegames/ui/wordle/WordleScreen.kt
@@ -3,7 +3,9 @@ package com.erm.composegames.ui.wordle
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -15,8 +17,10 @@ import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.graphics.Color.Companion.Yellow
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Preview
@@ -49,7 +53,9 @@ private fun WordleKeyboard(
     keyPressed: (WordleKey) -> Unit
 ) {
     Column(
-        modifier = Modifier.padding(2.dp),
+        modifier = Modifier
+            .padding(2.dp)
+            .fillMaxWidth(),
         verticalArrangement = Arrangement.spacedBy(2.dp)
     ) {
         keyboard.keys.forEach { keyRow ->
@@ -63,8 +69,12 @@ private fun WordleKeyboard(
                     Box(
                         modifier = Modifier
                             .border(1.dp, Black)
-                            .padding(4.dp)
                             .align(Alignment.CenterVertically)
+                            .clickable { keyPressed(key) }
+                            .padding(
+                                horizontal = 12.dp,
+                                vertical = 16.dp
+                            )
                             .background(
                                 when (key) {
                                     is WordleKey.WordleLetter.Green -> Green
@@ -81,6 +91,7 @@ private fun WordleKeyboard(
                                 is WordleKey.Delete -> "Delete"
                                 is WordleKey.Enter -> "Enter"
                             },
+                            fontSize = if (key is WordleKey.WordleLetter) 16.sp else 10.sp,
                             color = if (key is WordleKey.WordleLetter.Pending || key !is WordleKey.WordleLetter) Black else White
                         )
                     }
@@ -122,8 +133,11 @@ private fun WordleGrid(
                             )
                     ) {
                         Text(
-                            text = letter.char.toString(),
-                            color = if (letter is WordleKey.WordleLetter.Pending) Black else White
+                            text = letter.char?.toString() ?: "",
+                            color = if (letter is WordleKey.WordleLetter.Pending) Black else White,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.align(Alignment.Center),
+                            style = MaterialTheme.typography.h2
                         )
                     }
                 }
